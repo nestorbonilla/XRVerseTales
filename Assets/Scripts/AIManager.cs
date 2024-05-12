85e35d44d88defad1447e7675d1bd8468185be55
Nestor <nestor.bonilla.s@gmail.com>
2024-05-11 18:55:32 -0700

    adding capability to change image on portal from code

--- Assets/Scripts/AIManager.cs
+++ Assets/Scripts/AIManager.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 
 public class AIManager : MonoBehaviour
 {
-    public string imageFileName = "image-360-2.jpg"; // The name of the image file
-    private string imageFilePath; // The full path to the image file
+    public string imageFileName = "image-360-2.jpg";
+    private string imageFilePath;
     private Texture2D texture;
-    [SerializeField] private MeshRenderer sphereRenderer;
+    [SerializeField] private Material backgroundMaterial;
 
     void Start()
     {
-        // Get the full path of the image file within the Assets folder
         imageFilePath = Path.Combine(Application.dataPath, "Textures", imageFileName);
 
         if (File.Exists(imageFilePath))
         {
-            // Load the image as a byte array
             byte[] imageData = File.ReadAllBytes(imageFilePath);
-            texture = new Texture2D(2048, 1024); // Initialize with the exact width and height
+            texture = new Texture2D(2048, 1024);
             texture.LoadImage(imageData);
-            sphereRenderer.material.mainTexture = texture;
+            backgroundMaterial.mainTexture = texture;
         }
         else
         {
