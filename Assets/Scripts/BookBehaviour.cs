6cac3f76eb7a87ed602f939e7ae425a466a4791c
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-05-13 12:12:13 -0300

    MainScene complete

--- Assets/Scripts/BookBehaviour.cs
+++ Assets/Scripts/BookBehaviour.cs
@@ -3,27 +3,45 @@ using UnityEngine;
 
 public class BookBehaviour : MonoBehaviour
 {
-    [SerializeField] private GameObject pdfReader;
+    private const string OpenTrigger = "OpenTrigger";
+    private const string CloseTrigger = "CloseTrigger";
+
+    [SerializeField] private GameObject defaultPdfReader;
+    [SerializeField] private GameObject[] pdfReaders;
     [SerializeField] private Animator bookAnimator;
     [SerializeField] private float rotationSpeed = 1f;
-    [SerializeField] private Transform centerEyeAnchor;
-    
-    private static readonly int OpenTrigger = Animator.StringToHash("OpenTrigger");
-    private static readonly int CloseTrigger = Animator.StringToHash("CloseTrigger");
+    [SerializeField] private Transform cameraRig;
 
+    private bool isBookOpen = false;
     public void OpenBook()
     {
-        bookAnimator.SetTrigger(OpenTrigger);
-        StartCoroutine(LerpRotation(Quaternion.Euler(180, 180, 270)));
+        isBookOpen = true;
+        SetBookRotation();
+        TriggerBookAnimation(OpenTrigger);
         StartCoroutine(DelayedActivation(1.2f));
     }
 
     public void CloseBook()
     {
-        pdfReader.SetActive(false);
-        bookAnimator.SetTrigger(CloseTrigger);
+        isBookOpen = false;
+        foreach (GameObject pdfReader in pdfReaders)
+        {
+            pdfReader.SetActive(false);
+        }
+        TriggerBookAnimation(CloseTrigger);
     }
     
+    private void SetBookRotation()
+    {
+        float cameraYRotation = cameraRig.rotation.eulerAngles.y;
+        StartCoroutine(LerpRotation(Quaternion.Euler(180, cameraYRotation - 90, 270))); // Modify this line
+    }
+
+    private void TriggerBookAnimation(string trigger)
+    {
+        bookAnimator.SetTrigger(trigger);
+    }
+
     IEnumerator LerpRotation(Quaternion targetRotation)
     {
         while (Quaternion.Angle(transform.rotation, targetRotation) > 0.01f)
@@ -37,6 +55,6 @@ public class BookBehaviour : MonoBehaviour
     IEnumerator DelayedActivation(float delay)
     {
         yield return new WaitForSeconds(delay);
-        pdfReader.SetActive(true);
+        defaultPdfReader.SetActive(true);
     }
 }
